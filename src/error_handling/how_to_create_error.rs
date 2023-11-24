7e6e43a52d3cea9f4e48f5a98d0b153f9c6a8370
sweetim <hosweetim@gmail.com>
2023-11-24 11:05:04 +0900

    added how to create error

---
+++ src/error_handling/how_to_create_error.rs
@@ -0,0 +1,66 @@
+#[derive(Debug)]
+struct Account {
+    amount: u32,
+    total_withdrawal_limit: u32,
+}
+
+#[derive(Debug, PartialEq)]
+enum AccountError {
+    ExceedWithdrawalLimit,
+    InsufficientBalance(u32),
+}
+
+impl Account {
+    fn new(amount: u32, total_withdrawal_limit: u32) -> Self {
+        Self {
+            amount,
+            total_withdrawal_limit
+        }
+    }
+
+    fn withdraw(&mut self, amount: u32) -> Result<u32, AccountError> {
+        if amount > self.total_withdrawal_limit {
+            return Err(AccountError::ExceedWithdrawalLimit);
+        }
+
+        if let Some(new_amount) = self.amount.checked_sub(amount) {
+            self.amount = new_amount;
+            return Ok(self.amount);
+        } else {
+            return Err(AccountError::InsufficientBalance(self.amount));
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn withdraw_ok() {
+        let mut account = Account::new(1000, 100);
+        let actual = account.withdraw(50).unwrap();
+
+        assert_eq!(actual, 950);
+    }
+
+    #[test]
+    fn withdraw_withdrawal_limit_error() {
+        let mut account = Account::new(1000, 100);
+        let actual = account.withdraw(500);
+        assert!(actual.is_err());
+
+        let err = actual.unwrap_err();
+        assert_eq!(err, AccountError::ExceedWithdrawalLimit);
+    }
+
+    #[test]
+    fn withdraw_insuffient_error() {
+        let mut account = Account::new(10, 100);
+        let actual = account.withdraw(50);
+        assert!(actual.is_err());
+
+        let err = actual.unwrap_err();
+        assert_eq!(err, AccountError::InsufficientBalance(10));
+    }
+}
