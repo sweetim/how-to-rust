d29fcd050d6a4af1a97ad7434a95a137a067a3c7
sweetim <hosweetim@gmail.com>
2023-11-24 10:22:59 +0900

    added custom error using from trait

--- src/error_handling/using_custom_enum.rs
+++ src/error_handling/using_custom_enum.rs
@@ -17,6 +17,18 @@ impl Display for ReadStudentFromFileError {
 impl std::error::Error for ReadStudentFromFileError {
 }
 
+impl From<serde_json::Error> for ReadStudentFromFileError {
+    fn from(value: serde_json::Error) -> Self {
+        ReadStudentFromFileError::DeserializationError(value)
+    }
+}
+
+impl From<std::io::Error> for ReadStudentFromFileError {
+    fn from(value: std::io::Error) -> Self {
+        ReadStudentFromFileError::FileError(value)
+    }
+}
+
 fn read_student_from_file(path: &str) -> Result<Student, ReadStudentFromFileError> {
     let file = std::fs::File::open(&path)
         .map_err(ReadStudentFromFileError::FileError)?;
@@ -26,6 +38,14 @@ fn read_student_from_file(path: &str) -> Result<Student, ReadStudentFromFileErro
 
     let student = serde_json::from_str(&text)
         .map_err(ReadStudentFromFileError::DeserializationError)?;
+
+    Ok(student)
+}
+
+fn read_student_from_file_using_try(path: &str) -> Result<Student, ReadStudentFromFileError> {
+    let file = std::fs::File::open(&path)?;
+    let text = std::io::read_to_string(&file)?;
+    let student = serde_json::from_str(&text)?;
 
     Ok(student)
 }
@@ -43,6 +63,14 @@ fn read_student_from_file_with_dynamic_trait(path: &str) -> Result<Student, Box<
     Ok(student)
 }
 
+fn read_student_from_file_with_dynamic_trait_using_try(path: &str) -> Result<Student, Box<dyn std::error::Error>> {
+    let file = std::fs::File::open(&path)?;
+    let text = std::io::read_to_string(&file)?;
+    let student = serde_json::from_str(&text)?;
+
+    Ok(student)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -56,6 +84,14 @@ mod tests {
     }
 
     #[test]
+    fn it_will_error_read_student_from_file_using_try() {
+        let err = read_student_from_file_using_try("some.json")
+            .unwrap_err();
+
+        assert_eq!(format!("{}", err), "read student error (FileError(Os { code: 2, kind: NotFound, message: \"No such file or directory\" }))");
+    }
+
+    #[test]
     fn it_will_error_read_student_from_file_with_dynamic_trait() {
         let err = read_student_from_file_with_dynamic_trait("some.json")
             .unwrap_err();
@@ -63,6 +99,17 @@ mod tests {
         assert_eq!(format!("{}", err), "read student error (FileError(Os { code: 2, kind: NotFound, message: \"No such file or directory\" }))");
 
         let err = err.downcast_ref::<ReadStudentFromFileError>();
+        assert!(err.is_some());
+    }
+
+    #[test]
+    fn it_will_error_read_student_from_file_with_dynamic_trait_using_try() {
+        let err = read_student_from_file_with_dynamic_trait_using_try("some.json")
+            .unwrap_err();
+
+        // assert_eq!(format!("{}", err), "read student error (FileError(Os { code: 2, kind: NotFound, message: \"No such file or directory\" }))");
+
+        let err = err.downcast_ref::<std::io::Error>();
         assert!(err.is_some());
     }
 }
