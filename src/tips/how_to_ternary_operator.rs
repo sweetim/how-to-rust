8db17e768e52f8adacaf1a9c72d68429865385f9
sweetim <hosweetim@gmail.com>
2023-12-02 14:53:42 +0900

    refactor code

--- src/tips/how_to_ternary_operator.rs
+++ src/tips/how_to_ternary_operator.rs
@@ -1,34 +1,37 @@
-fn get_membership_fee_using_if_else_style_1(is_member: bool) -> f32 {
+const MEMBER_RATE: f32 = 10.0;
+const NON_MEMBER_RATE: f32 = 5.0;
+
+fn get_discount_rate_using_if_else_style_1(is_member: bool) -> f32 {
     if is_member {
-        10.0
+        MEMBER_RATE
     } else {
-        100.0
+        NON_MEMBER_RATE
     }
 }
 
-fn get_membership_fee_using_if_else_style_2(is_member: bool) -> f32 {
-    if is_member { 10.0 } else { 100.0 }
+fn get_discount_rate_using_if_else_style_2(is_member: bool) -> f32 {
+    if is_member { MEMBER_RATE } else { NON_MEMBER_RATE }
 }
 
-fn get_membership_fee_using_if_else_style_3(is_member: bool) -> f32 {
+fn get_discount_rate_using_if_else_style_3(is_member: bool) -> f32 {
     if is_member {
-        return 10.0;
+        return MEMBER_RATE;
     }
 
-    100.0
+    NON_MEMBER_RATE
 }
 
-fn get_membership_fee_using_match(is_member: bool) -> f32 {
+fn get_discount_rate_using_match(is_member: bool) -> f32 {
     match is_member {
-        true => 10.0,
-        false => 100.0
+        true => MEMBER_RATE,
+        false => NON_MEMBER_RATE
     }
 }
 
-fn get_membership_fee_using_chaining(is_member: bool) -> f32 {
+fn get_discount_rate_using_chaining(is_member: bool) -> f32 {
     is_member
-        .then(|| 10.0)
-        .unwrap_or_else(|| 100.0)
+        .then(|| MEMBER_RATE)
+        .unwrap_or_else(|| NON_MEMBER_RATE)
 }
 
 #[cfg(test)]
@@ -37,15 +40,15 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    #[case(false, 100.0)]
-    #[case(true, 10.0)]
+    #[case(false, NON_MEMBER_RATE)]
+    #[case(true, MEMBER_RATE)]
     fn it_should_all_get_same_value(#[case] is_member: bool, #[case] expected: f32) {
         let actual = vec![
-            get_membership_fee_using_if_else_style_1(is_member),
-            get_membership_fee_using_if_else_style_2(is_member),
-            get_membership_fee_using_if_else_style_3(is_member),
-            get_membership_fee_using_match(is_member),
-            get_membership_fee_using_chaining(is_member),
+            get_discount_rate_using_if_else_style_1(is_member),
+            get_discount_rate_using_if_else_style_2(is_member),
+            get_discount_rate_using_if_else_style_3(is_member),
+            get_discount_rate_using_match(is_member),
+            get_discount_rate_using_chaining(is_member),
         ];
 
         assert_eq!(actual, vec![ expected; actual.len() ]);
--- src/tips/mod.rs
+++ src/tips/mod.rs
@@ -1,4 +1,4 @@
-pub mod handling_writing_long_number;
+pub mod how_to_write_long_number;
 pub mod how_to_chain_method;
 pub mod how_to_input_multiple_test_sample;
 pub mod how_to_ternary_operator;
