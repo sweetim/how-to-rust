105dc72eefc63c65b8f432204e073e70c2b205ea
sweetim <hosweetim@gmail.com>
2023-12-04 23:00:01 +0900

    added faker to generate dummy data

--- src/tips/how_to_handle_large_test_sample.rs
+++ src/tips/how_to_handle_large_test_sample.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Default, Debug, PartialEq, Serialize, Deserialize)]
 pub struct UserIdentity {
     name: String,
     email: String,
@@ -8,49 +8,52 @@ pub struct UserIdentity {
 
 #[cfg(test)]
 mod tests {
+    use fake::Fake;
+    use rand::SeedableRng;
+
     use super::UserIdentity;
 
     fn get_samples() -> Vec<UserIdentity> {
         vec![
             UserIdentity {
-                name: String::from("Victor Kerr"),
-                email: String::from("lorem@protonmail.net"),
+                name: String::from("Rosalee Dare"),
+                email: String::from("eli@example.com"),
             },
             UserIdentity {
-                name: String::from("Irene Prince"),
-                email: String::from("odio.aliquam@outlook.couk"),
+                name: String::from("Cecilia Konopelski"),
+                email: String::from("caesar@example.net"),
             },
             UserIdentity {
-                name: String::from("Cameron Blair"),
-                email: String::from("amet.ante.vivamus@protonmail.org"),
+                name: String::from("Merritt Rohan"),
+                email: String::from("monty@example.net"),
             },
             UserIdentity {
-                name: String::from("Finn Petersen"),
-                email: String::from("dictum.magna@yahoo.com"),
+                name: String::from("Lily Heathcote"),
+                email: String::from("yolanda@example.org"),
             },
             UserIdentity {
-                name: String::from("Miriam Christensen"),
-                email: String::from("tincidunt.tempus.risus@outlook.couk"),
+                name: String::from("Nestor McCullough"),
+                email: String::from("jade@example.com"),
             },
             UserIdentity {
-                name: String::from("Carlos Wilkinson"),
-                email: String::from("tincidunt.adipiscing@protonmail.edu"),
+                name: String::from("Libbie Hahn"),
+                email: String::from("ron@example.com"),
             },
             UserIdentity {
-                name: String::from("Yoshi Mosley"),
-                email: String::from("fusce.aliquet@outlook.couk"),
+                name: String::from("Devyn Kuhlman"),
+                email: String::from("julie@example.org"),
             },
             UserIdentity {
-                name: String::from("Fay Holden"),
-                email: String::from("mauris.ipsum@yahoo.org"),
+                name: String::from("Madonna Doyle"),
+                email: String::from("alanna@example.com"),
             },
             UserIdentity {
-                name: String::from("Edward Todd"),
-                email: String::from("elementum.lorem@aol.edu"),
+                name: String::from("Frederik Goyette"),
+                email: String::from("candida@example.com"),
             },
             UserIdentity {
-                name: String::from("Axel Frost"),
-                email: String::from("lorem.fringilla@icloud.edu"),
+                name: String::from("Noemie Crist"),
+                email: String::from("corine@example.com"),
             },
         ]
     }
@@ -82,5 +85,24 @@ mod tests {
     #[test]
     fn store_test_sample_in_file_and_import_when_needed_at_compile_time() {
         assert_eq!(get_test_sample_from_file_at_compile_time(), get_samples());
+    }
+
+    fn generate_fake_sample_data() -> Vec<UserIdentity> {
+        let seed: [u8; 32] = [ 1; 32 ];
+        let ref mut rng = rand::rngs::StdRng::from_seed(seed);
+
+        (0..10).into_iter()
+            .map(|_| {
+                UserIdentity {
+                    name: fake::faker::name::en::Name().fake_with_rng(rng),
+                    email: fake::faker::internet::en::SafeEmail().fake_with_rng(rng),
+                }
+            })
+            .collect()
+    }
+
+    #[test]
+    fn it_faker() {
+        assert_eq!(generate_fake_sample_data(), get_samples());
     }
 }
