b777dde9a6d3a9c469fb055f720e198e18514d8c
sweetim <hosweetim@gmail.com>
2023-12-04 22:23:04 +0900

    make variable name more explicit

--- src/tips/how_to_write_string_variable.rs
+++ src/tips/how_to_write_string_variable.rs
@@ -2,19 +2,19 @@
 mod tests {
     #[test]
     fn it_same_string() {
-        let text_1 = "{
+        let normal_with_escape_text = "{
             \"name\": \"rust\",
             \"year\": 2023
         }";
 
-        let text_2 = r#"{
+        let raw_string_literal_text = r#"{
             "name": "rust",
             "year": 2023
         }"#;
 
-        let text_3 = include_str!("../../resources/string_variable.txt");
+        let macro_include_file_text = include_str!("../../resources/string_variable.txt");
 
-        assert_eq!(text_1, text_2);
-        assert_eq!(text_1, text_3);
+        assert_eq!(normal_with_escape_text, raw_string_literal_text);
+        assert_eq!(normal_with_escape_text, macro_include_file_text);
     }
 }
