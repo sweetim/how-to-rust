53ef0608ba157a82cbff718a95d4c1fd1224c4eb
sweetim <hosweetim@gmail.com>
2023-12-22 22:04:44 +0900

    refactor code

--- src/tips/how_to_have_pretty_assert.rs
+++ src/tips/how_to_have_pretty_assert.rs
@@ -18,8 +18,7 @@ struct CpuStates {
 mod tests {
     use super::*;
 
-    #[test]
-    fn with_normal_assertion() {
+    fn get_sample() -> (CpuStates, CpuStates) {
         let actual = CpuStates {
             cpu: 1,
             user: 2.0,
@@ -43,35 +42,20 @@ mod tests {
             soft_irq: 1.0,
             steal: 1.0,
         };
+
+        (actual, expected)
+    }
+
+    #[test]
+    fn with_normal_assertion() {
+        let (actual, expected) = get_sample();
 
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn with_pretty_assertion() {
-        let actual = CpuStates {
-            cpu: 1,
-            user: 2.0,
-            system: 3.0,
-            nice: 4.0,
-            idle: 5.0,
-            io_wait: 6.0,
-            hw_irq: 7.0,
-            soft_irq: 8.0,
-            steal: 9.0,
-        };
-
-        let expected = CpuStates {
-            cpu: 1,
-            user: 1.0,
-            system: 1.0,
-            nice: 1.0,
-            idle: 1.0,
-            io_wait: 1.0,
-            hw_irq: 1.0,
-            soft_irq: 1.0,
-            steal: 1.0,
-        };
+        let (actual, expected) = get_sample();
 
         pretty_assertions::assert_eq!(actual, expected);
     }
